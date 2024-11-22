1498e6b212bebfebc19044cfcdaab86bbbee01d6
raxusssssss <gwtgtggrtwgtgrgwrgtrwtg@gmail.com>
2024-11-21 19:23:50 -0600

    Welcome to Stellar.

--- public/js/cool.js
+++ public/js/cool.js
@@ -17,3 +17,28 @@ function switchTheme(theme) {
 
 // Example: Switch to green theme
 switchTheme('green');
+
+function addNewGame() {
+    var gameIconURL = prompt("Enter the URL for the game icon:");
+    var gameName = prompt("Enter the name of the game:");
+    var gameURL = prompt("Enter the URL for the game:");
+
+    if (gameIconURL && gameName && gameURL) {
+        var gamesContainer = document.querySelector('.games-container');
+        var newGameButton = document.createElement('div');
+        newGameButton.className = 'game-button';
+
+        var newGameLink = document.createElement('a');
+        newGameLink.href = gameURL;
+
+        var newGameImage = document.createElement('img');
+        newGameImage.src = gameIconURL;
+        newGameImage.alt = gameName + " Cover";
+
+        newGameLink.appendChild(newGameImage);
+        newGameButton.appendChild(newGameLink);
+        gamesContainer.appendChild(newGameButton);
+    } else {
+        alert("All fields are required to add a new game.");
+    }
+}
