949323f5a70eb35ab09601c2d262547944abae19
raxusssssss <gwtgtggrtwgtgrgwrgtrwtg@gmail.com>
2024-12-14 10:31:05 -0600

    Welcome to Stellar.
    VERSION 2.0 QUESTS UPDATE!!!!!!!!

--- public/password.js
+++ public/password.js
@@ -3,33 +3,8 @@ function promptPassword() {
     const correctPassword = 'cosmic123'; // Change this to your desired password
 
     if (password === correctPassword) {
-        window.location.href = '/t'; // Change this to your desired redirect URL
+          localStorage.setItem('quest3', 'true');
     } else {
         alert('Incorrect password. Please try again.');
-    }
-}
-
-function addNewGame() {
-    var gameIconURL = prompt("Enter the URL for the game icon:");
-    var gameName = prompt("Enter the name of the game:");
-    var gameURL = prompt("Enter the URL for the game:");
-
-    if (gameIconURL && gameName && gameURL) {
-        var gamesContainer = document.querySelector('.games-container');
-        var newGameButton = document.createElement('div');
-        newGameButton.className = 'game-button';
-
-        var newGameLink = document.createElement('a');
-        newGameLink.href = gameURL;
-
-        var newGameImage = document.createElement('img');
-        newGameImage.src = gameIconURL;
-        newGameImage.alt = gameName + " Cover";
-
-        newGameLink.appendChild(newGameImage);
-        newGameButton.appendChild(newGameLink);
-        gamesContainer.appendChild(newGameButton);
-    } else {
-        alert("All fields are required to add a new game.");
     }
 }
