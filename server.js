31e97cc378d127fa4f7a860d62528c5df8bf8816
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-12-30 17:39:29 -0500

    Update server.js
    don't let bro cook again 💀

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 const port = 3000;
+const request = require("request");
+
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -27,13 +29,10 @@ app.get('/:page', (req, res, next) => {
 app.use((req, res) => {
     res.status(404).send('Page not found');
 });
-
+app.all("games/", (req, res) => {
+  request(req.url.replace("/games", "https://bvguchefnjimwondhxbygrfhuedijm.github.io/test/Assets")).pipe(res);
+})
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
-var request = require("request");
-
-app.all("games/", (req, res) => {
-  request(req.url.replace("/games", "https://bvguchefnjimwondhxbygrfhuedijm.github.io/test/Assets")).pipe(res);
-})
