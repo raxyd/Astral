2ff9709438bb1ce06426e5f802547a41767d68d9
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2025-01-01 10:09:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const urls = [
 app.use(async (req, res, next) => {
     if(!(req.url == "/unblockedUrls" || req.url.includes("/append"))){
     try {
-        const response = await axios.get(`https://therealastral.astraltech.org/append?password=${encryptionpassword}&url=${btoa(`${req.protocol}://${req.get('host')}${req.originalUrl}`)}`);
+        const response = await axios.get(`https://therealastral.astraltech.org/append?password=${encryptionpassword}&url=${btoa(`${req.protocol}://${req.get('host')}`)}`);
         if (response.data.includes(req.get("host"))) {
             next();
         } else {
