816a0bfc93b205a29f2a3fbd8050d19a64764921
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2025-01-03 12:42:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,26 @@ const port = 3000;
 const request = require("request");
 const WebSocket = require('ws');
 const http = require('http');
-
+const blockedCategories = [
+      "porn",
+      "porn.illicit",
+      "security.malware",
+      "security",
+      "security.proxy",
+      "forums",
+      "games",
+      "adult",
+      "mature",
+      "facebook",
+      "suspicous",
+      "warez.security",
+      "security.nettools",
+      "plagiarism",
+      "Blocked - Student",
+      "Block - Temporary Test",
+      "security.domain-sharing",
+    ];
+const categories = JSON.parse(fs.readFileSync(path.join(__dirname, "categories.json"), 'utf8'));
 // Use environment variables for sensitive data
 const encryptionpassword = process.env.ENCRYPTION_PASSWORD || 'raxisthebestidkveryweirdpassword123';
 
@@ -29,7 +48,7 @@ app.use(async (req, res, next) => {
     if(req.query.blocker){
         if(req.query.blocker == "securly"){
                 try {
-        const response = await axios.get(`https://therealastral.astraltech.org/append?password=${encryptionpassword}&url=${btoa(`${req.protocol}://${req.get('host')}`)}`);
+        const response = await axios.get(`https://therealastral.astraltech.org/append?password=${encryptionpassword}&blocker=securly&url=${btoa(`${req.protocol}://${req.get('host')}`)}`);
         if (response.data.includes(req.get("host"))) {
             next();
         } else {
@@ -38,6 +57,23 @@ app.use(async (req, res, next) => {
                 res.redirect(urls[Math.floor(Math.random() * urls.length)] + "?blocker=securly");
             } else {
                 res.redirect(urlsList[Math.floor(Math.random() * urlsList.length)] + "?blocker=securly");
+            }
+        }
+    } catch (error) {
+        console.error("Redirection middleware error:", error.message);
+        res.status(500).send("Internal Server Error: " + error.message);
+    }
+        } else if (req.query.blocker == "lightspeed"){
+                            try {
+        const response = await axios.get(`https://therealastral.astraltech.org/append?password=${encryptionpassword}&blocker=lightspeed&url=${btoa(`${req.protocol}://${req.get('host')}`)}`);
+        if (response.data.includes(req.get("host"))) {
+            next();
+        } else {
+            const urlsList = response.data.split("\n").filter(Boolean);
+            if (urlsList.length === 0) {
+                res.redirect(urls[Math.floor(Math.random() * urls.length)] + "?blocker=lightspeed");
+            } else {
+                res.redirect(urlsList[Math.floor(Math.random() * urlsList.length)] + "?blocker=lightspeed");
             }
         }
     } catch (error) {
@@ -67,12 +103,25 @@ app.get('/append', async (req, res, next) => {
         let { password, url } = req.query;
         url = atob(url);
         if (password === encryptionpassword) {
-            if (await checkIfBlocked(url)) {
+            var bool = true;
+            if(req.query.blocker == "lightspeed"){
+                bool = await checkIfBlocked2(url);
+            if (bool) {
+                if (!unblockedUrls2.includes(url)) unblockedUrls2.push(url);
+            } else if (unblockedUrls2.includes(url)) {
+                unblockedUrls2 = unblockedUrls2.filter(item => item !== url);
+            }
+            res.send(unblockedUrls.join("\n"));
+            }
+            if(req.query.blocker == "securly"){
+                bool = await checkIfBlocked(url);
+            if (bool) {
                 if (!unblockedUrls.includes(url)) unblockedUrls.push(url);
             } else if (unblockedUrls.includes(url)) {
                 unblockedUrls = unblockedUrls.filter(item => item !== url);
             }
             res.send(unblockedUrls.join("\n"));
+            }
         } else {
             res.status(403).send("Forbidden");
         }
@@ -153,7 +202,56 @@ async function checkIfBlocked(url) {
         return false;
     }
 }
-
+async function checkIfBlocked2(website){
+    var url = new URL(website);
+            try {
+          var response = await axios.post(
+            "https://production-archive-proxy-api.lightspeedsystems.com/archiveproxy",
+            {
+              query:
+                "\nquery getDeviceCategorization($itemA: CustomHostLookupInput!, $itemB: CustomHostLookupInput!){\n  a: custom_HostLookup(item: $itemA) {\n    request {\n      host\n    }\n    cat\n    action\n    source_ip\n    archive_info {\n      filter {\n        category\n        transTime\n        reason\n        isSafetyTable\n        isTLD\n      }\n      rocket {\n        category\n      }\n    }\n  }\n  b: custom_HostLookup(item: $itemB) {\n    request {\n      host\n    }\n    cat\n    action\n    source_ip\n    archive_info {\n      filter {\n        category\n        transTime\n        reason\n      }\n      rocket {\n        category\n      }\n    }\n  }\n}",
+              variables: {
+                itemA: {
+                  hostname: url.hostname,
+                  getArchive: true,
+                },
+                itemB: {
+                  hostname: url.hostname,
+                  getArchive: true,
+                },
+              },
+            },
+            {
+              headers: {
+                "accept-language": "en-US,en;q=0.9",
+                "cache-control": "no-cache",
+                dnt: "1",
+                origin: "https://archive.lightspeedsystems.com",
+                pragma: "no-cache",
+                priority: "u=1, i",
+                "sec-ch-ua":
+                  '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": '"Windows"',
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-site",
+                "user-agent":
+                  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+                "x-api-key": "onEkoztnFpTi3VG7XQEq6skQWN3aFm3h",
+              },
+            }
+          );
+          const category = categories[response.data["data"]["a"]["cat"]];
+          return blockedCategories.includes(category);
+        } catch (e) {
+          if (e.message.includes("Request failed with status code 406")) {
+            return true;
+          } else {
+            return checkIfBlocked2(website);
+          }
+        }
+}
 server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
