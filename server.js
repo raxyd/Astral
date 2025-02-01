3b0c3f5ecc12ea62d86f57b20e2854c25123aa59
RAX <astraltechh@gmail.com>
2025-02-01 08:46:08 -0600

    ASTRAL.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { Analytics } from "@vercel/analytics/react"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
